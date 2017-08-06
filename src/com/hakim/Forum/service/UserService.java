3cad227a44962e1429e0e8410d70e2afa7e41568
hahmim <hahmim@gmail.com>
2017-08-05 23:24:28 -0400

    added the code for form regitration into remote

---
+++ src/com/hakim/Forum/service/UserService.java
@@ -0,0 +1,8 @@
+package com.hakim.Forum.service;
+
+import com.hakim.Forum.domain.User;
+
+public interface UserService {
+
+	public void add(User user);
+}
---
+++ src/com/hakim/Forum/service/UserServiceImpl.java
@@ -0,0 +1,13 @@
+package com.hakim.Forum.service;
+
+import com.hakim.Forum.domain.User;
+
+public class UserServiceImpl implements UserService {
+
+	public void add(User user) {
+		//Persist the user object here. 
+		System.out.println("User added successfully");
+
+	}
+
+}
